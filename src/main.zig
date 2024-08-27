142d6d96ebf189ba04d358c72d8ddfe3681514a9
joe-principe <j.principe343@gmail.com>
2024-08-27 00:47:24 -0700

    Some renaming. Working on movement functions

--- src/main.zig
+++ src/main.zig
@@ -3,15 +3,29 @@ const assert = std.debug.assert;
 
 const vaxis = @import("vaxis");
 
-const Player = enum(u1) {
+const Mark = enum(u1) {
     X,
     O,
 
     comptime {
-        for (0..std.enums.values(Player).len) |i| {
-            const res: Player = @enumFromInt(i);
+        for (0..std.enums.values(Mark).len) |i| {
+            const res: Mark = @enumFromInt(i);
             assert(@intFromEnum(res) == i);
         }
+    }
+};
+
+const Player = union(PlayerType) {
+    Local,
+    Computer: BotDifficulty,
+    Remote,
+
+    fn getLocalMove(board: Board) u8 {
+
+    }
+
+    fn getRemoteMove(board: Board) u8 {
+
     }
 };
 
@@ -37,6 +51,34 @@ const BotDifficulty = enum(u3) {
     ABPruning,
     PreCache,
 
+    fn getEasyMove(board: Board) u8 {
+
+    }
+
+    fn getMediumMove(board: Board) u8 {
+
+    }
+
+    fn getMinimaxMove(board: Board) u8 {
+
+    }
+
+    fn getCacheMove(board: Board) u8 {
+
+    }
+
+    fn getFastCacheMove(board: Board) u8 {
+
+    }
+
+    fn getABPruningMove(board: Board) u8 {
+
+    }
+
+    fn getPreCacheMove(board: Board) u8 {
+
+    }
+
     comptime {
         for (0..std.enums.values(PlayerType).len) |i| {
             const res: BotDifficulty = @enumFromInt(i);
@@ -60,10 +102,12 @@ const WinState = enum(u2) {
 };
 
 const Board = struct {
+    const Self = @This();
+
     x: u9,
     o: u9,
 
-    fn checkForWin(self: *Board, turn: u8) WinState {
+    fn checkForWin(self: *Self, turn: u8) WinState {
         const winning_boards = [_]u9{ 0b111_000_000, 0b000_111_000, 0b000_000_111, 0b100_100_100, 0b010_010_010, 0b001_001_001, 0b100_010_001, 0b001_010_100 };
 
         for (winning_boards) |board| {
@@ -83,7 +127,7 @@ const Board = struct {
         }
     }
 
-    fn isPositionOccupied(self: *Board, pos: u8) bool {
+    fn isPositionOccupied(self: *Self, pos: u8) bool {
         return if ((self.x & (1 << 9 - pos)) == 0 or (self.o & (1 << 9 - pos)) == 0) {
             false;
         } else {
@@ -91,12 +135,12 @@ const Board = struct {
         };
     }
 
-    fn placeMark(self: *Board, pos: u8, player: Player) void {
-        switch (player) {
-            Player.X => {
+    fn placeMark(self: *Self, pos: u8, mark: Mark) void {
+        switch (mark) {
+            Mark.X => {
                 self.x | (1 << 9 - pos);
             },
-            Player.O => {
+            Mark.O => {
                 self.o | (1 << 9 - pos);
             },
         }
@@ -104,20 +148,59 @@ const Board = struct {
 };
 
 const Game = struct {
+    const Self = @This();
+
     board: Board,
-    current_player: Player,
-    player_types: [2]PlayerType,
+    current_player: Mark,
+    players: [2]Player,
     turn: u8,
 
-    fn init(self: *Game) void {
+    fn init(self: *Self) void {
         self.board = .{ .x = 0, .o = 0 };
-        self.current_player = Player.X;
+        self.current_player = Mark.X;
+        self.players = undefined;
         self.turn = 1;
+    }
+
+    fn getMove(self: *Self) void {
+        switch (self.players[self.current_player]) {
+            .Local => {
+                Player.getLocalMove(self.board);
+            },
+            .Remote => {
+                Player.getRemoteMove(self.board);
+            },
+            .Computer => {
+                switch (self.players[self.current_player].BotDifficulty) {
+                    .Easy => {
+                        BotDifficulty.getEasyMove(self.board);
+                    },
+                    .Medium => {
+                        BotDifficulty.getMediumMove(self.board);
+                    },
+                    .Minimax => {
+                        BotDifficulty.getMinimaxMove(self.board);
+                    },
+                    .Cache => {
+                        BotDifficulty.getCacheMove(self.board);
+                    },
+                    .FastCache => {
+                        BotDifficulty.getFastCacheMove(self.board);
+                    },
+                    .ABPruning => {
+                        BotDifficulty.getABPruningMove(self.board);
+                    },
+                    .PreCache => {
+                        BotDifficulty.getPreCacheMove(self.board);
+                    },
+                }
+            }
+        }
     }
 };
 
 pub fn main() !void {
-    std.debug.print("Hello, world!\n", .{});
+    var game: Game = Game.init();
 }
 
 test "win_x" {
